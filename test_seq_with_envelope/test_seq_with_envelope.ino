916b9ccc9dbe53700ada2d2ac69b1adfbfca2e34
Ulysse Rosselet <urosselet@gmail.com>
2020-05-15 13:16:01 +0200

    pitched sequence and noise metronome

--- test_seq_with_envelope/test_seq_with_envelope.ino
+++ test_seq_with_envelope/test_seq_with_envelope.ino
@@ -11,57 +11,78 @@
 		https://groups.google.com/forum/#!forum/mozzi-users
 
 		Tim Barrass 2012, CC by-nc-sa
- */
+*/
 
 #include <MozziGuts.h>
 #include <Oscil.h> // oscillator template
 #include <tables/brownnoise8192_int8.h> // recorded audio wavetable
+#include <tables/sin2048_int8.h> // sine table for oscillator
 #include <Ead.h> // exponential attack decay
 #include <EventDelay.h>
 #include <mozzi_rand.h>
+#include <mozzi_midi.h>
+
 
 #define CONTROL_RATE 256 // Hz, powers of 2 are most reliable
 
-Oscil<BROWNNOISE8192_NUM_CELLS, AUDIO_RATE> aNoise(BROWNNOISE8192_DATA);
+// use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+Oscil <BROWNNOISE8192_NUM_CELLS, AUDIO_RATE> aNoise(BROWNNOISE8192_DATA);
 EventDelay kDelay; // for triggering envelope start
 Ead kEnvelope(CONTROL_RATE); // resolution will be CONTROL_RATE
+Ead nEnvelope(CONTROL_RATE); // resolution will be CONTROL_RATE
 
 int gain;
+int noiseGain;
 
-int seqGate[]={1,1,0,1,0};
+int seqGate[] = {1, 1, 0, 1, 0, 0, 1};
+int seqGateLength = 7;
+int seqPitch[] = {0, 4, 7, 12, 19, 16, 7, -5, -1};
+int seqPitchLength = 9;
 int seqIndex = 0;
+int pitchIndex = 0;
 
-void setup(){
+int baseNote = 60;
+
+void setup() {
   // use float to set freq because it will be small and fractional
-  aNoise.setFreq((float)AUDIO_RATE/BROWNNOISE8192_SAMPLERATE);
+  aNoise.setFreq((float)AUDIO_RATE / BROWNNOISE8192_SAMPLERATE);
+  aSin.setFreq(440); // set the frequency
   randSeed(); // fresh random, MUST be called before startMozzi - wierd bug
   startMozzi(CONTROL_RATE);
-  kDelay.start(500);
+  kDelay.start(200);
 }
 
 
-void updateControl(){
+void updateControl() {
   // jump around in audio noise table to disrupt obvious looping
   aNoise.setPhase(rand((unsigned int)BROWNNOISE8192_NUM_CELLS));
 
-  if(kDelay.ready()){
-    if(seqGate[seqIndex % sizeof(seqGate)] == 1){ 
-          kEnvelope.start(5,800);
-    } else {
+  if (kDelay.ready()) {
+
+    if (seqGate[seqIndex % seqGateLength] == 1) { // action if gate ON
+      kEnvelope.start(5, 600);
+      int note = baseNote + seqPitch[pitchIndex % seqPitchLength];
+      aSin.setFreq(mtof(float(note)));
+      pitchIndex ++;
+    } else { // action if gate OFF
     }
-    kDelay.start(500);
-    // move to next step
     seqIndex++;
+    nEnvelope.start(20, 10);
+    kDelay.start(200);
   }
   gain = (int) kEnvelope.next();
+  noiseGain = (int) nEnvelope.next();
 }
 
 
-int updateAudio(){
-  return (gain*aNoise.next())>>8;
+int updateAudio() {
+  int noiseChannel = noiseGain * aNoise.next()>>4;
+  int sineChannel = gain * aSin.next()>>1;
+  return (noiseChannel + sineChannel) >> 8;
 }
 
 
-void loop(){
+void loop() {
   audioHook(); // required here
 }
